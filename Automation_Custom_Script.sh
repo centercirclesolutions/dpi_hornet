b2eda44c6c3d1bdcf33071489a44a428f1acd1c9
Dave <dave.emde@centercirclesolutions.com>
2019-12-28 17:52:52 -0800

    Further Updates to messages and install

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -2,7 +2,7 @@
 # This script will install Hornet
 #The user can put config.json file in the /boot directory which will be copied into the hornet directory
 #This file should be installed at: /boot/Automation_Custom_Script.sh
-DPIH_BRANCH=`grep -Po '(?<=HORNET_BRANCH=)([A-Za-z0-9]+)' /boot/dietpi.txt`
+DPIH_BRANCH=`grep -Po '(?<=/dpi_hornet/)([A-Za-z0-9]+)(?=/Automation_Custom_Script.sh)' /boot/dietpi.txt`
 
 COLOUR_RESET='\e[0m'
 aCOLOUR=(
@@ -33,19 +33,16 @@ echo ""
 [[ $DPIH_BRANCH = "master" ]] && BC="${aCOLOUR[1]}" || BC="${aCOLOUR[3]}"
 echo -e '\e[90m[\e[0m INFO \e[90m]\e[0m Hornet   | Using Installer Branch ['$BC$DPIH_BRANCH'\e[0m]'
 
-echo -e '\e[90m[\e[0m INFO \e[90m]\e[0m Hornet   | Installing support software jq and less'
-
-#Apt Retry 3 times
+#Update APT to retry 3 times
 echo "APT::Acquire::Retries \"3\";" > "/etc/apt/apt.conf.d/80-retries"
 
 apt install -y jq less > /dev/null
 
-#Install Error handeling
 if ! [ -x "$(command -v jq)" ]; then
-	echo -e '[\e[31mFAILED\e[0m] Hornet | Failed to Load jq and less Hornet will not be installed...'
+	echo -e '[\e[31mFAILED\e[0m] Hornet   | Failed to Load jq and less. Skipping Hornet installation...'
     exit 1
 else
-	echo -e '[\e[32m  OK  \e[0m] Installed jq and less.'
+	echo -e '[\e[32m  OK  \e[0m] Hornet   | Installed jq and less.'
 fi
 
 #GLOBAL###############################################
@@ -54,49 +51,43 @@ HORNET_BIN="/opt/hornet"
 SERVICE_FILE="/etc/systemd/system/hornet.service"
 HORNETUSER=hornet
 ######################################################
-echo -e '\e[90m[\e[0m INFO \e[90m]\e[0m Hornet   | Creating user and group'
-adduser --system --group --no-create-home $HORNETUSER
+adduser --system --group --no-create-home --quiet $HORNETUSER
 mkdir $HORNET_SRC
 mkdir $HORNET_BIN
 mkdir $HORNET_BIN/config_history
 
-chown -R $HORNETUSER:$HORNETUSER $HORNET_BIN $HORNET_SRC && echo -e '[\e[32m  OK  \e[0m] Hornet user and group Created'
+chown -R $HORNETUSER:$HORNETUSER $HORNET_BIN $HORNET_SRC && echo -e '[\e[32m  OK  \e[0m] Hornet   | Hornet user and group Created'
  
-echo -e '\e[90m[\e[0m INFO \e[90m]\e[0m Hornet   | Getting the latest version'
 HORNETURL=`wget -q -nv -O- https://api.github.com/repos/gohornet/hornet/releases/latest 2>/dev/null |  jq -r '.assets[] | select(.browser_download_url | contains("Linux_ARM.")) | .browser_download_url'`
-echo -e '\e[90m[\e[0m INFO \e[90m]\e[0m Hornet   | Downloading Version: '$HORNETURL
+echo -e '\e[90m[\e[0m INFO \e[90m]\e[0m Hornet   | Downloading Latest Version: '$HORNETURL
 wget -Nqc --show-progress --progress=bar:force -O "/tmp/hornet-latest.tar.gz" $HORNETURL
-tar -xzf "/tmp/hornet-latest.tar.gz" -C $HORNET_SRC --strip-components 1 && rm /tmp/hornet-latest.tar.gz && echo -e '[\e[32m  OK  \e[0m] Hornet Installed'
+tar -xzf "/tmp/hornet-latest.tar.gz" -C $HORNET_SRC --strip-components 1 && rm /tmp/hornet-latest.tar.gz && echo -e '[\e[32m  OK  \e[0m] Hornet   | Hornet Installed'
 
 #Put latest version file
-[[ \$HORNETURL =~ .*(HORNET.+)\.tar\.gz  ]] && touch "\$HORNET_SRC/latestversion-\${BASH_REMATCH[1]}"
+[[ \$HORNETURL =~ .*(HORNET.+)\.tar\.gz  ]] && touch "$HORNET_SRC/latestversion-${BASH_REMATCH[1]}"
 
 echo -e '\e[90m[\e[0m INFO \e[90m]\e[0m Hornet   | Getting latest snapshot file... \e[1m(this might take a bit)\e[0m'
 
 wget -Nqc --show-progress --progress=bar:force -O "$HORNET_BIN/latest-export.gz.bin" https://dbfiles.iota.org/mainnet/hornet/latest-export.gz.bin
 
 if [ ! -f  "$HORNET_BIN/latest-export.gz.bin" ]; then
-	echo -e "There was a problem downloading the snapshot. ${aCOLOUR[3]}You will need to download the snapshot file after the restart$COLOUR_RESET"
+	echo -e '[\e[31mFAILED\e[0m] Hornet   | '"Failed to downloading snapshot. ${aCOLOUR[3]} use hn-snapshot to download it after reboot.$COLOUR_RESET"
 fi
 
 #Linking binary file
 ln -s $HORNET_SRC/hornet $HORNET_BIN/hornet
 
 if [ -f /boot/config.json ]; then
-	echo -e "Found the user supplied config.json file in /boot directory"
+	echo -e '[\e[32m  OK  \e[0m] Hornet   | Using user suppid config..json'
 	cp -f /boot/config.json $HORNET_BIN/config.json
 else
-	echo -e "Using the config.json file from github... ${aCOLOUR[3]}please edit after restart$COLOUR_RESET"
+	echo -e '[\e[31mFAILED\e[0m] Hornet   | Could not find config.json using hornet default'
 	cp $HORNET_SRC/config.json $HORNET_BIN/config.json
 fi
 
 #Change directories and files to hornet
 chown -R $HORNETUSER:$HORNETUSER $HORNET_BIN $HORNET_SRC
 
-#Setup SHR remount on startup
-#sed -i '/^tmpfs \/tmp tmpfs/s/^/#/' /etc/fstab
- 
-#	sed -i -- '/tmpfs \/DietPi/itmpfs /dev/shm tmpfs defaults,size=100M 0 0' /etc/fstab
 
 echo "Setting up Service"
 cat > $SERVICE_FILE <<EOF1
@@ -121,13 +112,11 @@ Group=$HORNETUSER
 WantedBy=multi-user.target
 EOF1
 
-echo "Enabling Services... Hornet Service will start on reboot"
-systemctl daemon-reload 
-systemctl enable hornet.service
+systemctl daemon-reload && systemctl enable hornet.service && echo -e '[\e[32m  OK  \e[0m] Hornet   | Configured and Enabled Hornet Service' || echo -e '[\e[31mFAILED\e[0m] Hornet   | Problem enabling Hornet Service'
 
 #Get Hornet script files
 mkdir /root/.hornet /home/dietpi/.hornet
-curl "https://raw.githubusercontent.com/centercirclesolutions/dpi_hornet/$DPIH_BRANCH/.hornet/{.bash_aliases,.bash_hornet}" -o "/root/.hornet/#1"
+curl -s "https://raw.githubusercontent.com/centercirclesolutions/dpi_hornet/$DPIH_BRANCH/.hornet/{.bash_aliases,.bash_hornet}" -o "/root/.hornet/#1" || echo -e '[\e[31mFAILED\e[0m] Hornet   | Failed to get Hornet admin scripts'
 cp /root/.hornet/* /home/dietpi/.hornet/
 chown -R dietpi:dietpi /home/dietpi/.hornet
 
